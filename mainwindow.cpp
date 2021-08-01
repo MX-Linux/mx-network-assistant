b1ad578e67f1d1d8cdc7eeac55f2e701248a79d0
Adrian <adrian@mxlinux.org>
2021-08-01 17:56:28 -0400

    Multiline comments for license headers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,21 +1,22 @@
-//
-//   Copyright (C) 2003-2010 by Warren Woodford
-//   Copyright (C) 2014 by Adrian adrian@mxlinux.org
-//
-//   Licensed under the Apache License, Version 2.0 (the "License");
-//   you may not use this file except in compliance with the License.
-//   You may obtain a copy of the License at
-//
-//       http://www.apache.org/licenses/LICENSE-2.0
-//
-//   Unless required by applicable law or agreed to in writing, software
-//   distributed under the License is distributed on an "AS IS" BASIS,
-//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//   See the License for the specific language governing permissions and
-//   limitations under the License.
-//
+/*
+   Copyright (C) 2003-2010 by Warren Woodford
+   Copyright (C) 2014 by Adrian adrian@mxlinux.org
 
-//   With big modifications made by Adrian adrian@mxlinux.org
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+   With big modifications made by Adrian adrian@mxlinux.org
+*/
 
 #include "mainwindow.h"
 #include "version.h"
