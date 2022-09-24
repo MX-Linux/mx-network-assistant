5149e04fcc111c6a8f4c88f020594d3bd4207fbc
Adrian <adrian@mxlinux.org>
2022-09-24 12:07:42 -0400

    Update translations
    Former-commit-id: 30e4dd21440ad3f173eeaf9edff197bda7004d4b
    Former-commit-id: c44623ba01a3eda2f1b0a674feb6d31cc3b9c2c4

--- translations/mx-network-assistant_nl_BE.ts
+++ translations/mx-network-assistant_nl_BE.ts
@@ -5,12 +5,12 @@
         <location filename="../datetime.ui" line="20"/>
         <location filename="../datetime.cpp" line="604"/>
         <source>MX Date &amp; Time</source>
-        <translation type="unfinished"/>
+        <translation>MX Datum &amp; Tijd</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="26"/>
         <source>Close</source>
-        <translation type="unfinished"/>
+        <translation>Sluiten</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="40"/>
@@ -20,251 +20,251 @@
     <message>
         <location filename="../datetime.ui" line="73"/>
         <source>Apply</source>
-        <translation type="unfinished"/>
+        <translation>Toepassen</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="100"/>
         <source>About...</source>
-        <translation type="unfinished"/>
+        <translation>Over...</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="124"/>
         <source>Date &amp;&amp; Time</source>
-        <translation type="unfinished"/>
+        <translation>Datum &amp;&amp; Tijd</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="192"/>
         <source>H:mm:ss</source>
-        <translation type="unfinished"/>
+        <translation>H:mm:ss</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="278"/>
         <location filename="../datetime.cpp" line="296"/>
         <location filename="../datetime.cpp" line="303"/>
         <source>Hardware Clock</source>
-        <translation type="unfinished"/>
+        <translation>Hardware Klok</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="284"/>
         <source>Hardware Clock tools</source>
-        <translation type="unfinished"/>
+        <translation>Hardware Klok gereedschappen</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="290"/>
         <source>Obtain information from the Hardware Clock and present it in the box below.</source>
-        <translation type="unfinished"/>
+        <translation>Verkrijg informatie van de Hardware Klok en presenteer dit in het vak hieronder.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="293"/>
         <source>Read the Hardware Clock</source>
-        <translation type="unfinished"/>
+        <translation>Lees de Hardware Klok</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="303"/>
         <source>Apply systematic drift corrections to the Hardware Clock.</source>
-        <translation type="unfinished"/>
+        <translation>Pas systematische drift correcties aan naar de Hardware Klok.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="306"/>
         <source>Drift Adjust</source>
-        <translation type="unfinished"/>
+        <translation>Drift Aanpassing</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="329"/>
         <source>Time transfer</source>
-        <translation type="unfinished"/>
+        <translation>Tijd overzetten</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="335"/>
         <source>Immediately set the System Clock to match the current time registered by the Hardware Clock.</source>
-        <translation type="unfinished"/>
+        <translation>Stel onmiddellijk de Systeem Klok in zodat deze gelijk is aan de huidige tijd geregistreerd door de hardware Klok.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="338"/>
         <source>Hardware Clock to System Clock</source>
-        <translation type="unfinished"/>
+        <translation>Hardware Klok naar Systeem Klok</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="348"/>
         <source>Immediately set the Hardware Clock to match the current time registered by the System Clock.</source>
-        <translation type="unfinished"/>
+        <translation>Stel onmiddellijk de Hardware Klok in zodat deze gelijk is aan de huidige tijd geregistreerd door de Systeem Klok.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="351"/>
         <source>System Clock to Hardware Clock</source>
-        <translation type="unfinished"/>
+        <translation>Systeem Klok naar Hardware Klok</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="361"/>
         <source>Update the drift factor for the Hardware Clock when it is being set.</source>
-        <translation type="unfinished"/>
+        <translation>Update de driftfaktor voor de Hardware Klok wanneer die ingesteld wordt.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="364"/>
         <source>Update the drift</source>
-        <translation type="unfinished"/>
+        <translation>Update de drift</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="374"/>
         <source>Time zone for the Hardware Clock</source>
-        <translation type="unfinished"/>
+        <translation>Tijdzone voor de  Hardware Klok</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="380"/>
         <source>Local time</source>
-        <translation type="unfinished"/>
+        <translation>Locale tijd</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="383"/>
         <source>Local</source>
-        <translation type="unfinished"/>
+        <translation>Lokaal</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="390"/>
         <source>Universally Coordinated Time (or Greenwich Meridian Time)</source>
-        <translation type="unfinished"/>
+        <translation>Universeel Gecoördineerde Tijd (of Greenwich Meridian Tijd)</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="393"/>
         <source>UTC</source>
-        <translation type="unfinished"/>
+        <translation>UTC</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="433"/>
         <source>Network Time</source>
-        <translation type="unfinished"/>
+        <translation>Netwerk Tijd</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="439"/>
         <source>Move the selected item down</source>
-        <translation type="unfinished"/>
+        <translation>Verplaats het geselecteerde onderdeel naar beneden</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="450"/>
         <source>Move the selected item up</source>
-        <translation type="unfinished"/>
+        <translation>Verplaats het geselecteerde onderdeel naar boven</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="461"/>
         <source>Remove the current item from the list</source>
-        <translation type="unfinished"/>
+        <translation>Verwijder het huidige onderdeel van de lijst</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="472"/>
         <source>Add an item to the list</source>
-        <translation type="unfinished"/>
+        <translation>Voeg een onderdeel aan de lijst toe</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="498"/>
         <source>Automatically update the System Clock with NTP servers:</source>
-        <translation type="unfinished"/>
+        <translation>Update de Systeem Klok automatisch naar de NTP servers:</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="505"/>
         <source>Immediately update the system clock with NTP servers.</source>
-        <translation type="unfinished"/>
+        <translation>Onmiddellijk de systeem klok updaten naar de NTP servers.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="508"/>
         <source>Update Now</source>
-        <translation type="unfinished"/>
+        <translation>Nu Updaten</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="520"/>
         <source>List of NTP servers to use.</source>
-        <translation type="unfinished"/>
+        <translation>Lijst van NTP servers om te gebruiken.</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="536"/>
         <source>Type</source>
-        <translation type="unfinished"/>
+        <translation>Typ</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="541"/>
         <source>Address</source>
-        <translation type="unfinished"/>
+        <translation>Adres</translation>
     </message>
     <message>
         <location filename="../datetime.ui" line="546"/>
         <source>Options</source>
-        <translation type="unfinished"/>
+        <translation>Opties</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="270"/>
         <source>Reading...</source>
-        <translation type="unfinished"/>
+        <translation>Lezen...</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="284"/>
         <source>Adjusting...</source>
-        <translation type="unfinished"/>
+        <translation>Aanpassen...</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="296"/>
         <location filename="../datetime.cpp" line="303"/>
         <source>System Clock</source>
-        <translation type="unfinished"/>
+        <translation>Systeem Klok</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="309"/>
         <source>The %1 time was transferred to the %2.</source>
-        <translation type="unfinished"/>
+        <translation>De %1 tijd was overgebracht naar de %2.</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="312"/>
         <source>The %1 time could not be transferred to the %2.</source>
-        <translation type="unfinished"/>
+        <translation>De %1 tijd kon niet worden overgebracht naar de %2.</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="352"/>
         <source>Updating...</source>
-        <translation type="unfinished"/>
+        <translation>Updaten...</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="365"/>
         <source>The system clock was updated successfully.</source>
-        <translation type="unfinished"/>
+        <translation>De systeem klok was succesvol geüpdate.</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="367"/>
         <source>The system clock could not be updated.</source>
-        <translation type="unfinished"/>
+        <translation>De systeem klok kon niet geüpdate worden.</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="369"/>
         <source>None of the NTP servers on the list are currently enabled.</source>
-        <translation type="unfinished"/>
+        <translation>Geen van de NTP servers op de lijst zijn momenteel ingeschakeld.</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="603"/>
         <source>About MX Date &amp; Time</source>
-        <translation type="unfinished"/>
+        <translation>Over MX Datum &amp; Tijd</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="604"/>
         <source>Version: </source>
-        <translation type="unfinished"/>
+        <translation>Versie:</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="605"/>
         <source>GUI program for setting the time and date in MX Linux</source>
-        <translation type="unfinished"/>
+        <translation>Grafisch programma om de tijd en datum in MX Linux in te stellen</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="607"/>
         <source>Copyright (c) MX Linux</source>
-        <translation type="unfinished"/>
+        <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="608"/>
         <location filename="../datetime.cpp" line="622"/>
         <source>License</source>
-        <translation type="unfinished"/>
+        <translation>Licentie</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="609"/>
         <source>Changelog</source>
-        <translation type="unfinished"/>
+        <translation>Changelog</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="610"/>
@@ -274,12 +274,12 @@
     <message>
         <location filename="../datetime.cpp" line="636"/>
         <source>&amp;Close</source>
-        <translation type="unfinished"/>
+        <translation>&amp;Sluiten</translation>
     </message>
     <message>
         <location filename="../datetime.cpp" line="654"/>
         <source>MX Date &amp; Time Help</source>
-        <translation type="unfinished"/>
+        <translation>MX Datum &amp; Tijd Hulp</translation>
     </message>
 </context>
 </TS>
