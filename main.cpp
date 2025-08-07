18751cea22ea7d24cc0ac86521af45b9f82fc85d
Adrian <adrian@mxlinux.org>
2025-08-07 15:31:49 -0400

    Don't set xcb if running on wayland

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     }
     // Set Qt platform to XCB (X11) if not already set and we're in X11 environment
     if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
-        if (!qEnvironmentVariableIsEmpty("DISPLAY")) {
+        if (!qEnvironmentVariableIsEmpty("DISPLAY") && qEnvironmentVariableIsEmpty("WAYLAND_DISPLAY")) {
             qputenv("QT_QPA_PLATFORM", "xcb");
         }
     }
